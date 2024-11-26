3b29e7d49e169065386aeba448411c6f1cb1928a
Encho Enevski <encho.enevski@gmail.com>
2024-11-26 11:21:16 +0200

    Add Initial project

---
+++ src/test/java/com/proxiad/oscars/OscarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxiad.oscars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
